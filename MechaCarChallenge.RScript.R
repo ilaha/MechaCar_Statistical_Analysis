ab35299dd64b44e72bc3775b7b1066b5bb51258a
ilaha mammadzada <ilaha.mammedzade@gmail.com>
2021-08-08 23:02:12 -0500

    Adding Linear Regression Prediction

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,8 +1,13 @@
 library(dplyr)
-
+library(tidyverse)
 MechaCar <- read_csv(file ='MechaCar_mpg.csv', check.names=F,stringsAsFactors = F)
 
 MechaCar_Reg = lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data = MechaCar)
 
 summary(MechaCar_Reg)
 
+Coil <- read.csv(file ='Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
+
+total_summary <-Coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+lot_summary <- Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
