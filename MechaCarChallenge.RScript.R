960d55f2d616faa11c4c56c3bd0ac2360a58bb85
ilaha mammadzada <ilaha.mammedzade@gmail.com>
2021-08-08 23:20:17 -0500

    Adding a T-test to the analysis

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -11,3 +11,18 @@ Coil <- read.csv(file ='Suspension_Coil.csv', check.names=F, stringsAsFactors = 
 total_summary <-Coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
 
 lot_summary <- Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+# T-test on Suspension Coils
+
+t.test(Coil$PSI,mu=mean(Coil$PSI))
+
+# T-test on Subsets:
+
+t.test(subset(Coil$PSI,Coil$Manufacturing_Lot == "Lot1"),mu=mean(Coil$PSI))
+
+t.test(subset(Coil$PSI,Coil$Manufacturing_Lot == "Lot2"),mu=mean(Coil$PSI))
+
+t.test(subset(Coil$PSI,Coil$Manufacturing_Lot == "Lot3"),mu=mean(Coil$PSI))
+
+
+
